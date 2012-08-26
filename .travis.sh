f7b306552d8dfd549702925e3c2f743e99da4d32
Dominic Cleal <dcleal@redhat.com>
2012-08-26 10:12:20 +0100

    Fix CI by not using Gemfile.lock, allow any version of Puppet in the same series and install Puppet modules with librarian-puppet or PMT (pre-2.7).

---
+++ .travis.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -xe
+
+# Use latest Augeas
+sudo add-apt-repository -y ppa:raphink/augeas
+sudo apt-get update
+sudo apt-get install augeas-tools libaugeas-dev libxml2-dev
+
+# Workaround for provider confinement on aliases
+sudo touch /etc/aliases
+
+# Install gems, but use the latest or those set by ENV not Gemfile.lock
+rm Gemfile.lock
+gem install bundler
+bundle install
+
+# Install puppet modules, preferably using librarian-puppet but that relies
+# on 2.7.12+ for `puppet module` so otherwise ignore and skip those specs
+if [[ $PUPPET_VERSION =~ ^(0.2|2.6) ]]; then
+    [ -d modules ] || mkdir modules
+    gem install puppet-module
+    ( cd modules && puppet-module install puppetlabs-mount_providers )
+else
+    librarian-puppet install
+fi
+
+# Reporting only
+bundle show
+puppet --version
+augtool --version
--- Gemfile
+++ Gemfile
@@ -1,16 +1,16 @@
 source :rubygems
 
 if ENV.key?('PUPPET_VERSION')
-  puppetversion = "= #{ENV['PUPPET_VERSION']}"
+  puppetversion = "~> #{ENV['PUPPET_VERSION']}"
 else
-  puppetversion = ['>= 2.7']
+  puppetversion = ['>= 0.25']
 end
 
-gem 'ruby-augeas', '>= 0.3.0'
 gem 'librarian-puppet'
+gem 'puppet', puppetversion
+gem 'ruby-augeas', '>= 0.3.0'
 
 group :development do
-  gem 'puppet', puppetversion
   gem 'puppet-lint'
   gem 'puppetlabs_spec_helper', '>= 0.2.0'
   gem 'rake'
