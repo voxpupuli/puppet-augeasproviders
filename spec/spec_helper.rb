df0c3e3f0d208a62390b335dc6566e23a7cc1697
Dominic Cleal <dcleal@redhat.com>
2012-08-26 10:16:05 +0100

    Exclude third party modules from spec coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,9 @@ $LOAD_PATH.unshift(dir, File.join(dir, 'lib'), File.join(dir, '..', 'lib'))
 require 'rubygems'
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  add_filter "/modules/"
+end
 
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'augeas_spec'
