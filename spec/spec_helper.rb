a4ffe680a176de5423b911deccf03b1bf558e950
Dominic Cleal <dcleal@redhat.com>
2012-09-23 21:34:08 +0100

    Test Puppet 3.0, add spec workaround, update gems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,3 +18,10 @@ RSpec.configure do |config|
 end
 
 Puppet[:modulepath] = File.join(dir, '..', 'modules')
+
+# Workaround for issue #16277 where default settings aren't initialised from a
+# spec and so the libdir is never initialised
+if Puppet.settings.respond_to? :app_defaults_initialized?
+  Puppet[:libdir] = Dir["#{Puppet[:modulepath]}/*/lib"].entries.join(File::PATH_SEPARATOR)
+end
+
