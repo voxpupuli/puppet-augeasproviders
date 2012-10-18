6f34df5f950d0d750bf9dc0b7d3e53ae93877402
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2012-10-18 11:21:03 +0200

    augeasproviders: new type nrpe_command

---
+++ lib/puppet/provider/nrpe_command/augeas.rb
@@ -0,0 +1,101 @@
+# Alternative Augeas-based provider for nrpe type
+#
+# Copyright (c) 2012 Christian Kaenzig
+# Licensed under the Apache License, Version 2.0
+
+require 'augeasproviders/provider'
+
+Puppet::Type.type(:nrpe_command).provide(:augeas) do
+  desc "Uses Augeas API to update nrpe commands"
+
+  include AugeasProviders::Provider
+
+  def self.file(resource = nil)
+    file = "/etc/nagios/nrpe.cfg"
+    file = resource[:target] if resource and resource[:target]
+    file.chomp("/")
+  end
+
+  confine :feature => :augeas
+  confine :exists => file
+
+  def self.augopen(resource = nil)
+    AugeasProviders::Provider.augopen("Nrpe.lns", file(resource))
+  end
+
+  def self.instances
+    aug = nil
+    begin
+      resources = []
+      aug = augopen
+      aug.match("/files#{file}/command/*").each do |spath|
+        resource = {:ensure => :present}
+
+        resource[:name] = spath.split("/")[-1]
+        resource[:value] = aug.get("#{spath}")
+
+        resources << new(resource)
+      end
+      resources
+    ensure
+      aug.close if aug
+    end
+  end
+
+  def exists? 
+    aug = nil
+    begin
+      aug = self.class.augopen(resource)
+      not aug.match("/files#{self.class.file(resource)}/command[.][./#{resource[:name]} =~ regexp('.*')]/#{resource[:name]}").empty?
+    ensure
+      aug.close if aug
+    end
+  end
+
+  def create 
+    aug = nil
+    begin
+      aug = self.class.augopen(resource)
+      aug.set("/files#{self.class.file(resource)}/command[%]/#{resource[:name]}", resource[:value])
+      augsave!(aug)
+    ensure
+      aug.close if aug
+    end
+  end
+
+  def destroy
+    aug = nil
+    begin
+      aug = self.class.augopen(resource)
+      aug.rm("/files#{self.class.file(resource)}/command[.][./#{resource[:name]} =~ regexp('.*')]/#{resource[:name]}")
+      augsave!(aug)
+    ensure
+      aug.close if aug
+    end
+  end
+
+  def target
+    self.class.file(resource)
+  end
+
+  def value
+    aug = nil
+    begin
+      aug = self.class.augopen(resource)
+      aug.get("/files#{self.class.file(resource)}/command[.][./#{resource[:name]} =~ regexp('.*')]/#{resource[:name]}")
+    ensure
+      aug.close if aug
+    end
+  end
+
+  def value=(value)
+    aug = nil
+    begin
+      aug = self.class.augopen(resource)
+      aug.set("/files#{self.class.file(resource)}/command[.][./#{resource[:name]} =~ regexp('.*')]/#{resource[:name]}", value)
+      augsave!(aug)
+    ensure
+      aug.close if aug
+    end
+  end
+end
