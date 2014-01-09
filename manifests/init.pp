bc1dbd727668320926c333cbaab65ede3bd11922
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-01-09 12:29:28 +0100

    Add dependencies for all types

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,4 +15,21 @@ class augeasproviders {
   gem { 'augeasproviders':
     ensure => 'installed',
   }
+
+  # Existing types
+  Gem['augeasproviders'] -> Host <||>
+  Gem['augeasproviders'] -> Mailalias <||>
+  Gem['augeasproviders'] -> Mounttab <||>
+
+  # Additional types
+  Gem['augeasproviders'] -> Apache_setenv <||>
+  Gem['augeasproviders'] -> Kernel_parameter <||>
+  Gem['augeasproviders'] -> Nrpe_command <||>
+  Gem['augeasproviders'] -> Pg_hba <||>
+  Gem['augeasproviders'] -> Puppet_auth <||>
+  Gem['augeasproviders'] -> Shellvar <||>
+  Gem['augeasproviders'] -> Sshd_config <||>
+  Gem['augeasproviders'] -> Sshd_config_subsystem <||>
+  Gem['augeasproviders'] -> Sysctl <||>
+  Gem['augeasproviders'] -> Syslog <||>
 }
