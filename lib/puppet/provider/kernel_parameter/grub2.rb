ca6be4801f1c3bdb030a4f6f1f56245375fec0a7
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-02-10 15:44:31 +0100

    kernel_parameter: fix grub2 provider for shared handler

--- lib/puppet/provider/kernel_parameter/grub2.rb
+++ lib/puppet/provider/kernel_parameter/grub2.rb
@@ -14,6 +14,10 @@ Puppet::Type.type(:kernel_parameter).provide(:grub2) do
 
   lens { 'Shellvars_list.lns' }
 
+  resource_path do |resource|
+    "$target/#{section(resource)}/value[.=~regexp('^#{resource[:name]}(=.*)?$')]"
+  end
+
   def self.mkconfig_path
     which("grub2-mkconfig") or which("grub-mkconfig") or '/usr/sbin/grub-mkconfig'
   end
@@ -22,11 +26,8 @@ Puppet::Type.type(:kernel_parameter).provide(:grub2) do
   commands :mkconfig => mkconfig_path
 
   def self.instances
-    aug = nil
-    path = "/files#{target}"
-    begin
+    augopen do |aug|
       resources = []
-      aug = augopen
 
       # Params are nicely separated, but no recovery-only setting (hard-coded)
       sections = { 'all'    => "GRUB_CMDLINE_LINUX",
@@ -34,13 +35,13 @@ Puppet::Type.type(:kernel_parameter).provide(:grub2) do
       sections.keys.sort.each do |bootmode|
         key = sections[bootmode]
         # Get all unique param names
-        params = aug.match("#{path}/#{key}/value").map { |pp|
+        params = aug.match("$target/#{key}/value").map { |pp|
           aug.get(pp).split("=")[0]
         }.uniq
 
         # Find all values for each param name
         params.each do |param|
-          vals = aug.match("#{path}/#{key}/value[.=~regexp('^#{param}(=.*)?$')]").map {|vp|
+          vals = aug.match("$target/#{key}/value[.=~regexp('^#{param}(=.*)?$')]").map {|vp|
             aug.get(vp).split("=", 2)[1]
           }
           vals = vals[0] if vals.size == 1
@@ -50,12 +51,10 @@ Puppet::Type.type(:kernel_parameter).provide(:grub2) do
         end
       end
       resources
-    ensure
-      aug.close if aug
     end
   end
 
-  def section
+  def self.section(resource)
     case resource[:bootmode].to_s
     when "normal"
       "GRUB_CMDLINE_LINUX_DEFAULT"
@@ -66,76 +65,33 @@ Puppet::Type.type(:kernel_parameter).provide(:grub2) do
     end
   end
 
-  def grubsave!(aug)
-    cfg = nil
-    ["/boot/grub/grub.cfg", "/boot/grub2/grub.cfg"].each {|c|
-      cfg = c if FileTest.file? c
-    }
-    fail("Cannot find grub.cfg location to use with grub-mkconfig") unless cfg
-
-    augsave! aug
-    mkconfig "-o", cfg
-  end
-
-  def exists?
-    aug = nil
-    path = "/files#{target}/#{section}"
-    begin
-      aug = self.class.augopen(resource)
-      !aug.match("#{path}/value[.=~regexp('^#{resource[:name]}(=.*)?$')]").empty?
-    ensure
-      aug.close if aug
-    end
-  end
-
   def create
     self.value=(resource[:value])
   end
 
-  def destroy
-    aug = nil
-    path = "/files#{target}/#{section}"
-    begin
-      aug = self.class.augopen(resource)
-      aug.rm("#{path}/value[.=~regexp('^#{resource[:name]}(=.*)?$')]")
-      grubsave!(aug)
-    ensure
-      aug.close if aug
-    end
-  end
-
   def value
-    aug = nil
-    path = "/files#{target}/#{section}"
-    begin
-      aug = self.class.augopen(resource)
-      aug.match("#{path}/value[.=~regexp('^#{resource[:name]}(=.*)?$')]").map {|vp|
+    augopen do |aug|
+      aug.match('$resource').map {|vp|
         aug.get(vp).split("=", 2)[1]
       }
-    ensure
-      aug.close if aug
     end
   end
 
   def value=(newval)
-    aug = nil
-    path = "/files#{target}/#{section}"
-    begin
-      aug = self.class.augopen(resource)
-
+    augopen! do |aug|
       if newval && !newval.empty?
         vals = newval.clone
       else
         # If no value (e.g. "quiet") then clear the value from the first and
         # delete the rest
         vals = nil
-        aug.set("#{path}/value[.=~regexp('^#{resource[:name]}(=.*)?$')][1]", resource[:name])
-        aug.rm("#{path}/value[.=~regexp('^#{resource[:name]}(=.*)?$')][position() > 1]")
+        aug.set("#{resource_path}[1]", resource[:name])
+        aug.rm("#{resource_path}[position() > 1]")
       end
 
       # Set any existing parameters with this name, remove excess ones
       if vals
-        aug.match("#{path}/value[.=~regexp('^#{resource[:name]}(=.*)?$')]").each do |ppath|
+        aug.match('$resource').each do |ppath|
           val = vals.shift
           if val.nil?
             aug.rm(ppath)
@@ -148,13 +104,20 @@ Puppet::Type.type(:kernel_parameter).provide(:grub2) do
       # Add new parameters where there are more values than existing params
       if vals && !vals.empty?
         vals.each do |val|
-          aug.set("#{path}/value[last()+1]", "#{resource[:name]}=#{val}")
+          aug.set("$target/#{self.class.section(resource)}/value[last()+1]", "#{resource[:name]}=#{val}")
         end
       end
-
-      grubsave!(aug)
-    ensure
-      aug.close if aug
     end
+  end
+
+  def flush
+    cfg = nil
+    ["/boot/grub/grub.cfg", "/boot/grub2/grub.cfg"].each {|c|
+      cfg = c if FileTest.file? c
+    }
+    fail("Cannot find grub.cfg location to use with grub-mkconfig") unless cfg
+    
+    augsave!(aug_handler) if using_post_resource_eval?
+    mkconfig "-o", cfg
   end
 end
