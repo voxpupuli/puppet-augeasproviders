82e4a62312a9ba8b0aaaebada7b600b70159abb7
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-03-12 13:14:04 +0100

    Cleanup, remove providers from core tree

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,24 +19,3 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 require 'augeas_spec'
 
 Puppet[:modulepath] = File.join(dir, 'fixtures', 'modules')
-
-# There's no real need to make this version dependent, but it helps find
-# regressions in Puppet
-#
-# 1. Workaround for issue #16277 where default settings aren't initialised from
-# a spec and so the libdir is never initialised (3.0.x)
-# 2. Workaround for 2.7.20 that now only loads types for the current node
-# environment (#13858) so Puppet[:modulepath] seems to get ignored
-# 3. Workaround for 3.5 where context hasn't been configured yet,
-# ticket https://tickets.puppetlabs.com/browse/MODULES-823
-#
-ver = Gem::Version.new(Puppet.version.split('-').first)
-if Gem::Requirement.new("~> 2.7.20") =~ ver || Gem::Requirement.new("~> 3.0.0") =~ ver || Gem::Requirement.new("~> 3.5") =~ ver
-  puts "augeasproviders: setting Puppet[:libdir] to work around broken type autoloading"
-  # libdir is only a single dir, so it can only workaround loading of one external module
-  Puppet[:libdir] = "#{Puppet[:modulepath]}/mount_providers/lib"
-  $LOAD_PATH.unshift(File.join(dir, 'fixtures/modules/stdlib/lib'))
-end
-
-# Load all shared contexts and shared examples
-Dir["#{dir}/support/**/*.rb"].sort.each {|f| require f}
