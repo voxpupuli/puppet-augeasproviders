7ba9db61ce678ae831917eb97fbfc95bc79fc7ea
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-06-18 09:11:00 +0200

    Shellvar: fix type specs

--- spec/unit/puppet/type/shellvar_type_spec.rb
+++ spec/unit/puppet/type/shellvar_type_spec.rb
@@ -3,20 +3,28 @@
 require 'spec_helper'
 
 describe Puppet::Type.type(:shellvar) do
-  describe "when setting quoted" do
+  context "when setting quoted" do
     it "should have a default of auto" do
-      resource = Puppet::Type.type(:shellvar).new :variable => "foo"
+      resource = Puppet::Type.type(:shellvar).new :name => "foo"
       resource[:quoted].should == :auto
     end
 
     it "should convert true to auto" do
-      resource = Puppet::Type.type(:shellvar).new :variable => "foo", :quoted => :true
+      resource = Puppet::Type.type(:shellvar).new :name => "foo", :quoted => :true
       resource[:quoted].should == :auto
     end
 
     it "should convert false to none" do
-      resource = Puppet::Type.type(:shellvar).new :variable => "foo", :quoted => :false
+      resource = Puppet::Type.type(:shellvar).new :name => "foo", :quoted => :false
       resource[:quoted].should == :none
+    end
+  end
+
+  context "when using composite namevar" do
+    it "should have a variable and target" do
+      resource = Puppet::Type.type(:shellvar).new :name => "foo in /tmp/bar"
+      resource[:variable].should == 'foo'
+      resource[:target].should == '/tmp/bar'
     end
   end
 end
