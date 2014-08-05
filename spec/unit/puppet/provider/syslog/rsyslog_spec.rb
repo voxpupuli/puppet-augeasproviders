711ed5515d74715233978c72f527e08b0f32bd78
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-08-05 15:55:45 +0200

    Fix rsyslog specs' reference to the mailalias type

--- spec/unit/puppet/provider/syslog/rsyslog_spec.rb
+++ spec/unit/puppet/provider/syslog/rsyslog_spec.rb
@@ -7,7 +7,7 @@ FileTest.stubs(:exist?).with('/etc/rsyslog.conf').returns true
 provider_class = Puppet::Type.type(:syslog).provider(:rsyslog)
 def valid_lens?
   # This lens breaks on Augeas 0.10.0
-  Puppet::Util::Package.versioncmp(Puppet::Type.type(:mailalias).provider(:augeas).aug_version, '0.10.0') > 0
+  Puppet::Util::Package.versioncmp(Puppet::Type.type(:syslog).provider(:augeas).aug_version, '0.10.0') > 0
 end
 
 describe provider_class, :if => valid_lens? do
