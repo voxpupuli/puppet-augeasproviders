b3f0705cf90b95cfc8b040479d5387c291bda876
Raphaël Pinson <raphael.pinson@camptocamp.com>
2014-08-06 10:02:21 +0200

    Use Coveralls formatter for SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ $LOAD_PATH.unshift(dir, File.join(dir, 'lib'), File.join(dir, '..', 'lib'))
 require 'rubygems'
 
 require 'simplecov'
+require 'coveralls'
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start do
   add_filter "/spec/fixtures/modules/mount_providers/"
   add_filter { |src_file|
@@ -40,6 +42,3 @@ end
 
 # Load all shared contexts and shared examples
 Dir["#{dir}/support/**/*.rb"].sort.each {|f| require f}
-
-require 'coveralls'
-Coveralls.wear!
