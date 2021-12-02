834ecadd6fd7b02ed6ce08818448cb0030be5b0f
Tim <tim@bastelfreak.de>
2021-12-02 14:20:12 +0100

    rubocop safe autofix

--- spec/classes/augeasproviders_instances_spec.rb
+++ spec/classes/augeasproviders_instances_spec.rb
@@ -1,30 +1,33 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe 'augeasproviders::instances' do
-  it { should create_class('augeasproviders::instances') }
-  it { should contain_class('augeasproviders::params') }
+  it { is_expected.to create_class('augeasproviders::instances') }
+  it { is_expected.to contain_class('augeasproviders::params') }
 
   context "when resource_defaults => 'foo'" do
-    let(:params) {{ :resource_defaults => 'foo' }}
-    it { expect { should create_class('augeasproviders::instances') }.to raise_error(Puppet::Error, /is not a Hash/) }
+    let(:params) { { resource_defaults: 'foo' } }
+
+    it { expect { is_expected.to create_class('augeasproviders::instances') }.to raise_error(Puppet::Error, %r{is not a Hash}) }
   end
 
-  [
-    'apache_directive',
-    'apache_setenv',
-    'host',
-    'kernel_parameter',
-    'mailalias',
-    'mounttab',
-    'nrpe_command',
-    'pam',
-    'pg_hba',
-    'puppet_auth',
-    'shellvar',
-    'sshd_config',
-    'sshd_config_subsystem',
-    'sysctl',
-    'syslog',
+  %w[
+    apache_directive
+    apache_setenv
+    host
+    kernel_parameter
+    mailalias
+    mounttab
+    nrpe_command
+    pam
+    pg_hba
+    puppet_auth
+    shellvar
+    sshd_config
+    sshd_config_subsystem
+    sysctl
+    syslog
   ].each do |type|
     describe "#{type} instances" do
       include_context type
