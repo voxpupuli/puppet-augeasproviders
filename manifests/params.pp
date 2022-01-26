b9cd0d697e4aa2a9b2bd0b4c2d8a98a5e895f268
Walter Kleynscheldt <walter.kleynscheldt@hetzner.co.za>
2022-01-26 17:11:50 +0200

    Change to using data types rather than type validation and switch to lookups rather than global variables

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,85 +4,101 @@
 #
 class augeasproviders::params {
 
-  $apache_directive_hash = $facts['augeasproviders_apache_directive_hash'] ? {
-    undef   => false,
-    default => $::augeasproviders_apache_directive_hash,
-  }
+  $apache_directive_hash = lookup({
+    'name'          => 'augeasproviders_apache_directive_hash',
+    'value_type'    => Hash,
+    'default_value' => {},
+  })
 
-  $apache_setenv_hash = $facts['augeasproviders_apache_setenv_hash'] ? {
-    undef   => false,
-    default => $::augeasproviders_apache_setenv_hash,
-  }
+  $apache_setenv_hash = lookup({
+    'name'          => 'augeasproviders_apache_setenv_hash',
+    'value_type'    => Hash,
+    'default_value' => {},
+  })
 
-  $host_hash = $facts['augeasproviders_host_hash'] ? {
-    undef   => false,
-    default => $::augeasproviders_host_hash,
-  }
+  $host_hash = lookup({
+    'name'          => 'augeasproviders_host_hash',
+    'value_type'    => Hash,
+    'default_value' => {},
+  })
 
-  $kernel_parameter_hash = $facts['augeasproviders_kernel_parameter_hash'] ? {
-    undef   => false,
-    default => $::augeasproviders_kernel_parameter_hash,
-  }
+  $kernel_parameter_hash = lookup({
+    'name'          => 'augeasproviders_kernel_parameter_hash',
+    'value_type'    => Hash,
+    'default_value' => {},
+  })
 
-  $mailalias_hash = $facts['augeasproviders_mailalias_hash'] ? {
-    undef   => false,
-    default => $::augeasproviders_mailalias_hash,
-  }
+  $mailalias_hash = lookup({
+    'name'          => 'augeasproviders_mailalias_hash',
+    'value_type'    => Hash,
+    'default_value' => {},
+  })
 
-  $mounttab_hash = $facts['augeasproviders_mounttab_hash'] ? {
-    undef   => false,
-    default => $::augeasproviders_mounttab_hash,
-  }
+  $mounttab_hash = lookup({
+    'name'          => 'augeasproviders_mounttab_hash',
+    'value_type'    => Hash,
+    'default_value' => {},
+  })
 
-  $nrpe_command_hash = $facts['augeasproviders_nrpe_command_hash'] ? {
-    undef   => false,
-    default => $::augeasproviders_nrpe_command_hash,
-  }
+  $nrpe_command_hash = lookup({
+    'name'          => 'augeasproviders_nrpe_command_hash',
+    'value_type'    => Hash,
+    'default_value' => {},
+  })
 
-  $pam_hash = $facts['augeasproviders_pam_hash'] ? {
-    undef   => false,
-    default => $::augeasproviders_pam_hash,
-  }
+  $pam_hash = lookup({
+    'name'          => 'augeasproviders_pam_hash',
+    'value_type'    => Hash,
+    'default_value' => {},
+  })
 
-  $pg_hba_hash = $facts['augeasproviders_pg_hba_hash'] ? {
-    undef   => false,
-    default => $::augeasproviders_pg_hba_hash,
-  }
+  $pg_hba_hash = lookup({
+    'name'          => 'augeasproviders_pg_hba_hash',
+    'value_type'    => Hash,
+    'default_value' => {},
+  })
 
-  $puppet_auth_hash = $facts['augeasproviders_puppet_auth_hash'] ? {
-    undef   => false,
-    default => $::augeasproviders_puppet_auth_hash,
-  }
+  $puppet_auth_hash = lookup({
+    'name'          => 'augeasproviders_puppet_auth_hash',
+    'value_type'    => Hash,
+    'default_value' => {},
+  })
 
-  $shellvar_hash = $facts['augeasproviders_shellvar_hash'] ? {
-    undef   => false,
-    default => $::augeasproviders_shellvar_hash,
-  }
+  $shellvar_hash = lookup({
+    'name'          => 'augeasproviders_shellvar_hash',
+    'value_type'    => Hash,
+    'default_value' => {},
+  })
 
-  $ssh_config_hash = $facts['augeasproviders_ssh_config_hash'] ? {
-    undef   => false,
-    default => $::augeasproviders_ssh_config_hash,
-  }
+  $ssh_config_hash = lookup({
+    'name'          => 'augeasproviders_ssh_config_hash',
+    'value_type'    => Hash,
+    'default_value' => {},
+  })
 
-  $sshd_config_hash = $facts['augeasproviders_sshd_config_hash'] ? {
-    undef   => false,
-    default => $::augeasproviders_sshd_config_hash,
-  }
+  $sshd_config_hash = lookup({
+    'name'          => 'augeasproviders_sshd_config_hash',
+    'value_type'    => Hash,
+    'default_value' => {},
+  })
 
-  $sshd_config_subsystem_hash = $facts['augeasproviders_sshd_config_subsystem_hash'] ? {
-    undef   => false,
-    default => $::augeasproviders_sshd_config_subsystem_hash,
-  }
+  $sshd_config_subsystem_hash = lookup({
+    'name'          => 'augeasproviders_sshd_config_subsystem_hash',
+    'value_type'    => Hash,
+    'default_value' => {},
+  })
 
-  $sysctl_hash = $facts['augeasproviders_sysctl_hash'] ? {
-    undef   => false,
-    default => $::augeasproviders_sysctl_hash,
-  }
+  $sysctl_hash = lookup({
+    'name'          => 'augeasproviders_sysctl_hash',
+    'value_type'    => Hash,
+    'default_value' => {},
+  })
 
-  $syslog_hash = $facts['augeasproviders_syslog_hash'] ? {
-    undef   => false,
-    default => $::augeasproviders_syslog_hash,
-  }
+  $syslog_hash = lookup({
+    'name'          => 'augeasproviders_syslog_hash',
+    'value_type'    => Hash,
+    'default_value' => {},
+  })
 
   $defaults = {
     'ensure'    => 'present',
